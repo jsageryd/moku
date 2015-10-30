3a3c6bb82033804ab708c5bf6fae3f41ea3c2c76
Johan Sageryd <j@1616.se>
2015-10-31 00:18:13 +0100

    Initial commit

---
+++ moku.go
@@ -0,0 +1,238 @@
+package moku
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+	"sync"
+)
+
+// Mux is the router/muxer. Create an instance of Mux using New().
+type Mux struct {
+	sync.RWMutex
+	rootNode *node
+
+	/*
+	   ConcurrentAdd (default true) can be set to false if routes will not be
+	   added while the router is serving requests, for higher throughput. Setting
+	   this to false will avoid taking a read lock on the routes tree on each
+	   request in the assumption that its tree is not being concurrently altered.
+	*/
+	ConcurrentAdd bool
+
+	/*
+	   RedirectTrailingSlash (default true) controls whether or not redirection
+	   occurs if a request is made to a route that matches it except for the
+	   trailing slash. If true and /foo is defined, a request to /foo/ will be
+	   redirected to /foo. If /foo/ is defined, a request made to /foo will be
+	   redirected to /foo/. If both /foo and /foo/ are defined, no redirection
+	   occurs.
+	*/
+	RedirectTrailingSlash bool
+}
+
+type node struct {
+	nodes   map[string]*node
+	handler http.HandlerFunc
+}
+
+func newNode() *node {
+	return &node{
+		nodes: make(map[string]*node),
+	}
+}
+
+// New creates a new Mux with default configuration.
+func New() *Mux {
+	return &Mux{
+		rootNode: newNode(),
+
+		ConcurrentAdd:         true,
+		RedirectTrailingSlash: true,
+	}
+}
+
+// Delete configures a DELETE route.
+func (m *Mux) Delete(path string, handler http.HandlerFunc) error {
+	return m.addRoute("DELETE", path, handler)
+}
+
+// Get configures a GET route.
+func (m *Mux) Get(path string, handler http.HandlerFunc) error {
+	return m.addRoute("GET", path, handler)
+}
+
+// Head configures a HEAD route.
+func (m *Mux) Head(path string, handler http.HandlerFunc) error {
+	return m.addRoute("HEAD", path, handler)
+}
+
+// Options configures an OPTIONS route.
+func (m *Mux) Options(path string, handler http.HandlerFunc) error {
+	return m.addRoute("OPTIONS", path, handler)
+}
+
+// Patch configures a PATCH route.
+func (m *Mux) Patch(path string, handler http.HandlerFunc) error {
+	return m.addRoute("PATCH", path, handler)
+}
+
+// Post configures a POST route.
+func (m *Mux) Post(path string, handler http.HandlerFunc) error {
+	return m.addRoute("POST", path, handler)
+}
+
+// Put configures a PUT route.
+func (m *Mux) Put(path string, handler http.HandlerFunc) error {
+	return m.addRoute("PUT", path, handler)
+}
+
+// Trace configures a TRACE route.
+func (m *Mux) Trace(path string, handler http.HandlerFunc) error {
+	return m.addRoute("TRACE", path, handler)
+}
+
+var errNoLeadingSlash = errors.New("Path does not being with leading slash")
+
+func (m *Mux) addRoute(method string, path string, handler http.HandlerFunc) error {
+	if m.ConcurrentAdd {
+		m.Lock()
+		defer m.Unlock()
+	}
+	if path[0] != '/' {
+		return errNoLeadingSlash
+	}
+
+	currentNode, ok := m.rootNode.nodes[method]
+	if !ok {
+		currentNode = newNode()
+		m.rootNode.nodes[method] = currentNode
+	}
+	splitString(path[1:], "/", func(part string) error {
+		t := currentNode.nodes
+		child, ok := t[part]
+		if !ok {
+			child = newNode()
+			t[part] = child
+		}
+		currentNode = child
+		return nil
+	})
+	currentNode.handler = handler
+	return nil
+}
+
+func (m *Mux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h, isRedirect := m.findHandlerFunc(r)
+	if h == nil {
+		if isRedirect {
+			var code int
+			if r.Method == "GET" {
+				code = http.StatusMovedPermanently
+			} else {
+				code = http.StatusTemporaryRedirect
+			}
+			http.Redirect(w, r, r.URL.String(), code)
+		} else {
+			http.NotFound(w, r)
+		}
+	} else {
+		h(w, r)
+	}
+}
+
+var errDeadEnd = errors.New("Dead end")
+
+func (m *Mux) findHandlerFunc(r *http.Request) (http.HandlerFunc, bool) {
+	if m.ConcurrentAdd {
+		m.RLock()
+		defer m.RUnlock()
+	}
+	var node, lastNode *node
+	var ok bool
+	nextNodeCandidates := m.rootNode.nodes
+	node, ok = nextNodeCandidates[r.Method]
+	if ok {
+		nextNodeCandidates = node.nodes
+	} else {
+		return nil, false
+	}
+	path := r.URL.Path[1:]
+	err := splitString(path, "/", func(part string) error {
+		lastNode = node
+		node, ok = nextNodeCandidates[part]
+		if ok {
+			nextNodeCandidates = node.nodes
+		} else {
+			return errDeadEnd
+		}
+		return nil
+	})
+	if m.RedirectTrailingSlash && (node == nil || node.handler == nil) {
+		return nil, setRedirectURL(r, node, lastNode)
+	}
+	if err == errDeadEnd {
+		return nil, false
+	}
+	return node.handler, false
+}
+
+func setRedirectURL(r *http.Request, node, lastNode *node) bool {
+	path := r.URL.Path
+	if path[len(path)-1] == '/' {
+		if lastNode != nil && lastNode.handler != nil {
+			r.URL.Path = path[:len(path)-1]
+			return true
+		}
+	} else {
+		if node != nil {
+			trailingNode, ok := node.nodes[""]
+			if ok && trailingNode.handler != nil {
+				r.URL.Path = path + "/"
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func splitString(s string, delimiter string, callback func(string) error) error {
+	start := 0
+	d := delimiter[0]
+	for i := 0; i < len(s); i++ {
+		if s[i] == d {
+			err := callback(s[start:i])
+			if err != nil {
+				return err
+			}
+			start = i + 1
+		}
+	}
+	return callback(s[start:])
+}
+
+// PrintRoutes prints the hierarchy of configured routes.
+func (m *Mux) PrintRoutes() {
+	type pathItem struct {
+		name   string
+		node   *node
+		indent int
+	}
+	var item *pathItem
+	var stack []*pathItem
+	for name, node := range m.rootNode.nodes {
+		stack = append(stack, &pathItem{name, node, 0})
+	}
+	for len(stack) > 0 {
+		item, stack = stack[len(stack)-1], stack[:len(stack)-1]
+		hasHandlerStr := "  "
+		if item.node.handler != nil {
+			hasHandlerStr = "* "
+		}
+		fmt.Printf("%s%s%s\n", hasHandlerStr, strings.Repeat("  ", item.indent), item.name)
+		for name, node := range item.node.nodes {
+			stack = append(stack, &pathItem{"/" + name, node, item.indent + 1})
+		}
+	}
+}
---
+++ moku_test.go
@@ -0,0 +1,301 @@
+package moku
+
+import (
+	"errors"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func assertStatus(t *testing.T, mux *Mux, method string, path string, status int) {
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(method, path, nil)
+	mux.ServeHTTP(w, req)
+	if w.Code != status {
+		t.Errorf("Expected HTTP %d, got %d", status, w.Code)
+	}
+}
+
+func assertHeader(t *testing.T, mux *Mux, method string, path string, headerKey string, headerValue string) {
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(method, path, nil)
+	mux.ServeHTTP(w, req)
+	gotHeaderValue := w.Header().Get(headerKey)
+	if gotHeaderValue != headerValue {
+		t.Errorf("Expected header %s to be \"%s\", was \"%s\"", headerKey, headerValue, gotHeaderValue)
+	}
+}
+
+func assertBodyEquals(t *testing.T, mux *Mux, method string, path string, expectedBody string) {
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(method, path, nil)
+	mux.ServeHTTP(w, req)
+	if w.Body.String() != expectedBody {
+		t.Errorf("Expected %s to return \"%s\", got \"%s\"", path, expectedBody, w.Body.String())
+	}
+}
+
+func TestGetWithoutLeadingSlash(t *testing.T) {
+	mux := New()
+	path := "foo"
+	err := mux.Get(path, func(w http.ResponseWriter, r *http.Request) {})
+	if err != errNoLeadingSlash {
+		t.Errorf("Expected errNoLeadingSlash, got %s", err)
+	}
+	assertStatus(t, mux, "GET", path, http.StatusNotFound)
+}
+
+func TestGetWithUnknownPath(t *testing.T) {
+	mux := New()
+	mux.Get("/foo", func(w http.ResponseWriter, r *http.Request) {})
+	assertStatus(t, mux, "GET", "/bar", http.StatusNotFound)
+}
+
+func TestDelete(t *testing.T) {
+	mux := New()
+	path := "/foo"
+	mux.Delete(path, func(w http.ResponseWriter, r *http.Request) {})
+	assertStatus(t, mux, "DELETE", path, http.StatusOK)
+}
+
+func TestGet(t *testing.T) {
+	mux := New()
+	path := "/foo"
+	mux.Get(path, func(w http.ResponseWriter, r *http.Request) {})
+	assertStatus(t, mux, "GET", path, http.StatusOK)
+}
+
+func TestHead(t *testing.T) {
+	mux := New()
+	path := "/foo"
+	mux.Head(path, func(w http.ResponseWriter, r *http.Request) {})
+	assertStatus(t, mux, "HEAD", path, http.StatusOK)
+}
+
+func TestOptions(t *testing.T) {
+	mux := New()
+	path := "/foo"
+	mux.Options(path, func(w http.ResponseWriter, r *http.Request) {})
+	assertStatus(t, mux, "OPTIONS", path, http.StatusOK)
+}
+
+func TestPatch(t *testing.T) {
+	mux := New()
+	path := "/foo"
+	mux.Patch(path, func(w http.ResponseWriter, r *http.Request) {})
+	assertStatus(t, mux, "PATCH", path, http.StatusOK)
+}
+
+func TestPost(t *testing.T) {
+	mux := New()
+	path := "/foo"
+	mux.Post(path, func(w http.ResponseWriter, r *http.Request) {})
+	assertStatus(t, mux, "POST", path, http.StatusOK)
+}
+
+func TestPut(t *testing.T) {
+	mux := New()
+	path := "/foo"
+	mux.Put(path, func(w http.ResponseWriter, r *http.Request) {})
+	assertStatus(t, mux, "PUT", path, http.StatusOK)
+}
+
+func TestTrace(t *testing.T) {
+	mux := New()
+	path := "/foo"
+	mux.Trace(path, func(w http.ResponseWriter, r *http.Request) {})
+	assertStatus(t, mux, "TRACE", path, http.StatusOK)
+}
+
+func newMuxWithGetPaths(paths []string) *Mux {
+	mux := New()
+	for _, path := range paths {
+		mux.Get(path, func(p string) http.HandlerFunc {
+			return func(w http.ResponseWriter, r *http.Request) {
+				io.WriteString(w, p)
+			}
+		}(path))
+	}
+	return mux
+}
+
+func newMuxWithPostPaths(paths []string) *Mux {
+	mux := New()
+	for _, path := range paths {
+		mux.Post(path, func(p string) http.HandlerFunc {
+			return func(w http.ResponseWriter, r *http.Request) {
+				io.WriteString(w, p)
+			}
+		}(path))
+	}
+	return mux
+}
+
+func TestMuxStaticSimple(t *testing.T) {
+	paths := []string{
+		"/",
+		"/foo",
+		"/foo/bar",
+		"/bar/",
+		"/bar/foo/",
+	}
+	mux := newMuxWithGetPaths(paths)
+	mux.RedirectTrailingSlash = false
+
+	for _, path := range paths {
+		assertStatus(t, mux, "GET", path, http.StatusOK)
+		expectedBody := path
+		assertBodyEquals(t, mux, "GET", path, expectedBody)
+	}
+}
+
+func TestMuxStaticNotFound(t *testing.T) {
+	mux := New()
+	mux.RedirectTrailingSlash = false
+	nilHandlerPath := "/nilhandler"
+	mux.Get(nilHandlerPath, nil)
+	assertStatus(t, mux, "GET", nilHandlerPath, http.StatusNotFound)
+	assertStatus(t, mux, "GET", "/undefined", http.StatusNotFound)
+}
+
+func TestMuxStaticRedirectTrailingSlashGet(t *testing.T) {
+	mux := newMuxWithGetPaths([]string{
+		"/",
+		"/foo",
+		"/foo/bar",
+		"/bar/",
+		"/bar/foo/",
+	})
+	mux.Get("/nilhandler", nil)
+	mux.RedirectTrailingSlash = true
+
+	expectations := []struct {
+		requestedPath        string
+		expectedRedirectPath string
+		expectedCode         int
+	}{
+		{"/", "", http.StatusOK},
+		{"/foo", "", http.StatusOK},
+		{"/foo/", "/foo", http.StatusMovedPermanently},
+		{"/foo/bar", "", http.StatusOK},
+		{"/foo/bar/", "/foo/bar", http.StatusMovedPermanently},
+		{"/bar", "/bar/", http.StatusMovedPermanently},
+		{"/bar/", "", http.StatusOK},
+		{"/bar/foo", "/bar/foo/", http.StatusMovedPermanently},
+		{"/bar/foo/", "", http.StatusOK},
+		{"/nilhandler", "", http.StatusNotFound},
+		{"/undefined", "", http.StatusNotFound},
+	}
+	for _, e := range expectations {
+		assertStatus(t, mux, "GET", e.requestedPath, e.expectedCode)
+		assertHeader(t, mux, "GET", e.requestedPath, "Location", e.expectedRedirectPath)
+	}
+}
+
+func TestMuxStaticRedirectTrailingSlashPost(t *testing.T) {
+	mux := newMuxWithPostPaths([]string{
+		"/",
+		"/foo",
+		"/foo/bar",
+		"/bar/",
+		"/bar/foo/",
+	})
+	mux.Post("/nilhandler", nil)
+	mux.RedirectTrailingSlash = true
+
+	expectations := []struct {
+		requestedPath        string
+		expectedRedirectPath string
+		expectedCode         int
+	}{
+		{"/", "", http.StatusOK},
+		{"/foo", "", http.StatusOK},
+		{"/foo/", "/foo", http.StatusTemporaryRedirect},
+		{"/foo/bar", "", http.StatusOK},
+		{"/foo/bar/", "/foo/bar", http.StatusTemporaryRedirect},
+		{"/bar", "/bar/", http.StatusTemporaryRedirect},
+		{"/bar/", "", http.StatusOK},
+		{"/bar/foo", "/bar/foo/", http.StatusTemporaryRedirect},
+		{"/bar/foo/", "", http.StatusOK},
+		{"/nilhandler", "", http.StatusNotFound},
+		{"/undefined", "", http.StatusNotFound},
+	}
+	for _, e := range expectations {
+		assertStatus(t, mux, "POST", e.requestedPath, e.expectedCode)
+		assertHeader(t, mux, "POST", e.requestedPath, "Location", e.expectedRedirectPath)
+	}
+}
+
+func TestSplitString(t *testing.T) {
+	stringSplits := map[string][]string{
+		"":           {""},
+		"/":          {"", ""},
+		"//":         {"", "", ""},
+		"/foo":       {"", "foo"},
+		"foo":        {"foo"},
+		"foo/":       {"foo", ""},
+		"/foo/":      {"", "foo", ""},
+		"/foo/bar":   {"", "foo", "bar"},
+		"/foo/bar/":  {"", "foo", "bar", ""},
+		"/foo/bar//": {"", "foo", "bar", "", ""},
+	}
+	delimiter := "/"
+	for str, expectedSplit := range stringSplits {
+		gotSplit := []string{}
+		splitString(str, delimiter, func(part string) error {
+			gotSplit = append(gotSplit, part)
+			return nil
+		})
+
+		gotStdLibSplit := strings.Split(str, delimiter)
+		if !splitSlicesEqual(gotSplit, gotStdLibSplit) {
+			t.Errorf("Split %q => %q, strings.Split says it should be %q", str, gotSplit, gotStdLibSplit)
+		}
+
+		if !splitSlicesEqual(gotSplit, expectedSplit) {
+			t.Errorf("Split %q => %q, expected %q", str, gotSplit, expectedSplit)
+		}
+	}
+}
+
+func TestSplitStringWithBreak(t *testing.T) {
+	gotSplit := []string{}
+	err := splitString("foo/bar/baz", "/", func(part string) error {
+		gotSplit = append(gotSplit, part)
+		return errors.New("foo")
+	})
+	if err == nil {
+		t.Errorf("Split did not break")
+	}
+	if len(gotSplit) != 1 {
+		t.Errorf("Expected break after first element, got %d elements: %q", len(gotSplit), gotSplit)
+	}
+}
+
+func splitSlicesEqual(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for n := range a {
+		if a[n] != b[n] {
+			return false
+		}
+	}
+	return true
+}
+
+func BenchmarkMuxStaticSimple(b *testing.B) {
+	mux := New()
+	mux.Get("/foo", func(w http.ResponseWriter, r *http.Request) {})
+	for n := 0; n < b.N; n++ {
+		r, err := http.NewRequest("GET", "/foo", nil)
+		if err != nil {
+			panic(err)
+		}
+		b.StartTimer()
+		mux.ServeHTTP(nil, r)
+		b.StopTimer()
+	}
+}
